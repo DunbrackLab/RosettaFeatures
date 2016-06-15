edefc3e7d2d83f924db9a11e7f7503f0eec02311
Jared Adolf-Bryfogle <jadolfbr@gmail.com>
2016-06-15 12:40:06 -0500

    add a script to run the features by passing a configuration file.

--- install_from_dunbrack_github.R
+++ install_from_dunbrack_github.R
@@ -1,4 +1,7 @@
 #!/usr/bin/env Rscript
 
+#NOTE: You must run this script with SUDO!!
+
+
 #install.packages("devtools", repos="http://cran.rstudio.com/")
 devtools::install_github("DunbrackLab/RosettaFeatures")

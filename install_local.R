912b36c03ea53613d2dc2b55e27832f326937ce4
Jared Adolf-Bryfogle <jadolfbr@gmail.com>
2016-09-14 12:01:51 -0500

    update gitignore.  remove Rprofile and Renviron from PR.

--- install_local.R
+++ install_local.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 
+
 #NOTE: You must run this script with SUDO!!
 
 #install.packages("devtools", repos="http://cran.rstudio.com/")
@@ -12,7 +13,7 @@ if (length(setdiff(required_packages, rownames(installed.packages()))) > 0) {
 
 library(methods)
 
-devtools::document() # if you changed function signatures
+#devtools::document() # if you changed function signatures
 devtools::build()
 
 devtools::install_local(".")
